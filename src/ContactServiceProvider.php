a4af38f3a5637f5b04dc869205ade22f86df69e8
Mohit Tiwari <mohit.tiwari@pebblemark.in>
2023-01-27 19:30:25 +0530

    Some changes fixed.

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -14,8 +14,10 @@ class ContactServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/config/contact.php','contact');
         
         $this->publishes([
-            __DIR__.'/config/contact.php'=>config_path('contact.php'),
-            __DIR__.'/views'=>resource_path('views/vendor/contact')            
+            __DIR__.'/config/contact.php'=>config_path('contact.php')           
+        ]);
+        $this->publishes([
+            __DIR__.'/views'=>resource_path('views/vendor/contact')
         ]);
     }
 
